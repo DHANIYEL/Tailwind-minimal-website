cdad959bc162dcecb6b9cf8ff5d2963aadd90c38
Dhaniyel Darvesh <dhaniyeldarvesh7@gmail.com>
2023-11-08 15:55:58 -1200

    Input the components

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ const App = () => (
     Nav
     <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
     <section className="padding">Popular product</section>
-    <section className="padding">Popular product</section>
-    <section className="padding">Popular product</section>
-    <section className="padding">Popular product</section>
+    <section className="padding">Siper Quality</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding">SpecialOffers</section>
+    <section className="padding bg-pale-blue">Customer review</section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section className="padding-x padding-t pb-8 bg-black text-white">
+      Footer
+    </section>
   </main>
 );
 
