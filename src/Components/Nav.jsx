39c630f2edcd34ddfab548c522d9df49716501a0
Dhaniyel Darvesh <dhaniyeldarvesh7@gmail.com>
2023-11-08 16:28:00 -1200

    page unload error

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,5 +1,16 @@
+import { headerLogo } from "../assets/images";
+import { Hamberger } from "../assets/icons";
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <header className="padding-x w-full py-8">
+      <nav>
+        <a href="#">
+          <img src={headerLogo} alt="Logo" width={130} height={29} />
+        </a>
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
