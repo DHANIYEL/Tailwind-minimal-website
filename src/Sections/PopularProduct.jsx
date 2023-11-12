5a815893f61b2e8c62455ed3606d515a12d2a6e1
Dhaniyel Darvesh <dhaniyeldarvesh7@gmail.com>
2023-11-11 19:43:09 -1200

    popular product page h2 span

--- src/Sections/PopularProduct.jsx
+++ src/Sections/PopularProduct.jsx
@@ -1,5 +1,19 @@
 const PopularProduct = () => {
-  return <div>PopularProduct</div>;
+  return (
+    <section id="product" className=" max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="font-palanquin text-4xl  font-bold">
+          Our<span className="text-coral-red">Popular</span> Products
+        </h2>
+        <p>
+          Introducing the latest innovation in athletic footwear – the Nike
+          .Engineered for peak performance and designed with style in mind,
+          these shoes represent the epitome of cutting-edge sports technology
+          and fashion-forward aesthetics.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default PopularProduct;
