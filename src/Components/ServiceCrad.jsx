493c0850405ce57ca3ac64b9113651742eca2f59
Dhaniyel Darvesh <dhaniyeldarvesh7@gmail.com>
2023-11-11 22:00:13 -1200

    service section finish

--- src/Components/ServiceCrad.jsx
+++ src/Components/ServiceCrad.jsx
@@ -4,6 +4,10 @@ const ServiceCrad = ({ imgURL, label, subtext }) => {
       <div className="w-11 h-11 bg-coral-red rounded-full flex justify-center items-center">
         <img src={imgURL} alt={label} width={24} height={24} />
       </div>
+      <h3 className="text-3xl leading-normal font-bold font-palanquin mt-5">
+        {label}
+      </h3>
+      <p className="mt-3 text-slate-gray font-montserrat">{subtext}</p>
     </div>
   );
 };
