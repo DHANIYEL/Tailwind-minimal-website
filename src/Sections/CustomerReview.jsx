7b9a31e23118535dd7db141bf4f954dad23c962e
Dhaniyel Darvesh <dhaniyeldarvesh7@gmail.com>
2023-11-11 23:18:00 -1200

    ReviewCard

--- src/Sections/CustomerReview.jsx
+++ src/Sections/CustomerReview.jsx
@@ -1,5 +1,24 @@
+import { reviews } from "../constants";
+import ReviewCard from "../Components/ReviewCard";
 const CustomerReview = () => {
-  return <div>CustomerReview</div>;
+  return (
+    <section className="max-container ">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="text-coral-red"> Customers </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto max-w-lg mt-4 text-center">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default CustomerReview;
