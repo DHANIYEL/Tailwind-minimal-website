30d10df76e6c86934f44ca0f16db822ddbb34abe
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2023-11-28 16:37:33 +0530

    flex row in small dwevices

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => (
     <section className="padding">
       <SuperQuality />
     </section>
-    <section className="padding-x py-10">
+    <section className="padding-x py-10 ">
       <Services />
     </section>
     <section className="padding">
--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -3,7 +3,7 @@ import { services } from "../Constants";
 
 const Services = () => {
   return (
-    <section className="max-container flex justify-center flex-wrap gap-9">
+    <section className="max-container sm:flex-row flex justify-center flex-wrap gap-9">
       {services.map((service, index) => (
         <ServiceCrad key={index} {...service} />
       ))}
